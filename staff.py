876025daf65d5449b5329a0eb7b3147cbee57efe
Amy McCormack <mccormack.amy@gmail.com>
2025-10-31 15:14:59 +1030

    Add files with updated header information.

--- staff.py
+++ staff.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
+File: staff.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Amellia (Amy) McCormack
+ID: 110392134
+Username: MCCAY044
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
