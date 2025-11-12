d461d569c80fa96308b3291156c615e00a54dfca
Amy McCormack <mccormack.amy@gmail.com>
2025-11-12 14:37:27 +1030

    Add docstrings to HealthRecord class, finished setter methods, and added behavioural methods issue_resolved and update_treatment_plan.

--- healthrecord.py
+++ healthrecord.py
@@ -8,14 +8,38 @@ This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 class HealthRecord:
+    """
+    Represents a health record for an animal.
+    The Health Record class models a single instance for a single animal, to be attached to that animals
+    health history.
+    """
+
+    # Four categories for health issues recorded
     ISSUE_TYPE = ['Injury', 'Illness', 'Behavioural', 'Other']
 
     def __init__(self, issue_type, severity_level, date_reported, description, treatment_plan):
-        self.issue_type = issue_type
-        self.__severity_level = severity_level
-        self.__date_reported = date_reported
-        self.__description = description
-        self.__is_current = True
+        """
+        Initialises a new Health Record instance.
+
+        Args:
+            issue_type (str): The category of issue being reported.
+            severity_level (int): The severity level from 0-3.
+            date_reported (str): The date of the initial report.
+            description (str): A description of the health issue
+            treatment_plan (str): The initial treatment plan/notes.
+
+        Attributes:
+            __issue_type (str): The category of issue being reported.
+            __severity_level (int): The severity level from 0-3 (Negligible, Minor, Moderate, Severe).
+            __date_reported (str): The date of the initial report.
+            __description (str): Description of the health issue.
+            __treatment_plan = (list[str]): Treatment plan and notes, including subsequent updates.
+        """
+        self.issue_type = issue_type                #Utilises the setter for validation of new instances
+        self.severity_level = severity_level        #Utilises the setter for validation of new instances
+        self.date_reported = date_reported          #Utilises the setter for validation of new instances
+        self.description = description              #Utilises the setter for validation of new instances
+        self.is_current = True
         self.__treatment_plan = [treatment_plan]
 
     # --------------
@@ -23,21 +47,27 @@ class HealthRecord:
     # --------------
 
     def get_issue_type(self)->str:
+        """Returns the health issue type."""
         return self.__issue_type
 
     def get_severity_level(self)->int:
+        """Returns the severity level of the health issue."""
         return self.__severity_level
 
     def get_date_reported(self)->str:
+        """Returns the date that the health issue was reported."""
         return self.__date_reported
 
     def get_description(self)->str:
+        """Returns a description of the health issue."""
         return self.__description
 
     def get_is_current(self)->bool:
+        """Returns whether the health issue is current (True) or resolved (False)."""
         return self.__is_current
 
     def get_treatment_plan(self)->list:
+        """Returns the list of treatment plan notes."""
         return self.__treatment_plan
 
     # --------------
@@ -45,31 +75,87 @@ class HealthRecord:
     # --------------
 
     def set_issue_type(self, issue_type):
+        """
+        Sets the health issue type.
+
+        Args:
+            type (str): The type of health issue being recorded. Must be a valid type.
+        """
         if issue_type in HealthRecord.ISSUE_TYPE:
             self.__issue_type = issue_type
         else:
-            print(f"Please enter a valid issue type from the following list:\n"
+            print(f"Invalid issue type. Please enter one of the following types:\n"
                   f"{HealthRecord.ISSUE_TYPE}")
 
     def set_severity_level(self, level):
+        """
+        Updates the severity level, ensuring it remains within a valid range.
+
+        Args:
+            severity_level (int): The new severity level (0-3).
+        """
         MIN_LEVEL = 0
         MAX_LEVEL = 3
         if isinstance(level, int) and MIN_LEVEL <= level <= MAX_LEVEL:
             self.__severity_level = level
         else:
-            print(f"Please enter")
+            print(f"Invalid severity level. Please enter an integer between {MIN_LEVEL} and {MAX_LEVEL}.")
 
+    # This would be better with an imported date file to ensure valid date rather just str.
     def set_date_reported(self, date):
-        pass
+        """
+        Updates the date of the report.
+
+        Args:
+            date_reported (str): The date that the issue was reported, ensuring only a string of a minimum
+            length may be passed.
+        """
+        MIN_LENGTH = 8
+        if isinstance(date, str) and len(date) >= MIN_LENGTH:
+            self.__date_reported = date
+        else:
+            print(f"Invalid date. Please enter text of at least {MIN_LENGTH} characters.")
 
     def set_description(self, description):
-        pass
+        """
+        Updates the description of the health issue.
+
+        Args:
+            description (str): Sets the description of the health issue, ensuring only a string of a minimum
+            length may be passed.
+        """
+        MIN_LENGTH = 12
+        if isinstance(description, str) and len(description) >= MIN_LENGTH:
+            self.__description = description
+        else:
+            print(f"Invalid description. Please enter text of at least {MIN_LENGTH} characters.")
 
     def set_is_current(self, is_current):
-        pass
+        """
+        Updates the status of the health issue (current = True, resolved = False).
+
+        Args:
+            is_current (bool): The status of the health issue.
+        """
+        if isinstance(is_current, bool):
+            self.__is_current = is_current
+        else:
+            print(f"Invalid current health status. Please enter either True (issue still current) or "
+                  f"False (issue resolved).")
 
     def set_treatment_plan(self, treatment_plan):
-        pass
+        """
+        Sets the initial treatment plan/notes.
+
+        Args:
+            treatment_plan (str): The treatment plan/notes for the health issue, ensuring only a string
+             of a minimum length may be passed.
+        """
+        MIN_LENGTH = 12
+        if isinstance(treatment_plan, str) and len(treatment_plan) >= MIN_LENGTH:
+            self.__treatment_plan = [treatment_plan]
+        else:
+            print(f"Invalid treatment plan. Please enter text of at least {MIN_LENGTH} characters.")
 
     # --------------------
     # Property definitions
@@ -86,5 +172,21 @@ class HealthRecord:
     # Behavioural methods
     # -------------------
 
+    def issue_resolved(self):
+        """Marks the health issue as resolved."""
+        self.is_current = False
 
+    def update_treatment_plan(self, treatment_plan):
+        """
+        Updates the treatment plan with further notes.
+
+        Args:
+            treatment_plan (str): The treatment plan/notes for the health issue, ensuring only a string
+             of a minimum length may be passed.
+        """
+        MIN_LENGTH = 12
+        if isinstance(treatment_plan, str) and len(treatment_plan) >= MIN_LENGTH:
+            self.__treatment_plan.append(treatment_plan)
+        else:
+            print(f"Invalid treatment plan. Please enter text of at least {MIN_LENGTH} characters.")
 
